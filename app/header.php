2e3818747cab912b328a77939a400320414f10b3
Izumra <vladimirkondaurov38@gmail.com>
2022-12-04 17:16:43 +0500

    mark2

--- app/header.php
+++ app/header.php
@@ -1,5 +1,5 @@
 <header>
-    <div class="header-content">
+    <div class="header-content container">
         <div class="header-content__search-block">
             <a href="/" class="search-block__logo">OrgEvent</a>
             <input class="search-block__search" type="text" placeholder="Поиск">
@@ -9,7 +9,7 @@
             <button class="account-block__log-in">Войти</button>
             <?php else: ?>
                 <div class="account-block__user-name"><?= $_COOKIE['user-surname'] .' '. $_COOKIE['user-name']; ?></div>
-                <a href="app/include/exit.php">
+                <a class="link-btn" href="app/include/exit.php">
                     <button class="account-block__log-out">Выйти</button>
                 </a>
             <?php endif;?>
--- contest.php
+++ contest.php
@@ -12,7 +12,7 @@
         <main>
             <?php require('app/header.php');?>
 
-            <section class="contest">
+            <!-- <section class="waits-zapusk">
                 <div class="container">
                     <div class="events__list">
                         <div class="events__item event-item">
@@ -23,16 +23,29 @@
                             <div class="event-item__status">Статус: <span></span></div>
                         </div>
                         <div class="content-description">
-                            <div class="description">
-
-                            </div>
+                            <div class="description">&nbsp; В рамках данной тематики участники должны предоставить работу, отражающую возможности человечества, направленные на созидание и улучшение благосостояния окружающей среды. В качестве доказательства о проделанной работе участник должны предоставить видеоматериал в формате .avi или .mp4, или текстовые отчеты в формате .docx или .pdf. Призовой фонд составляет 200 000 рублей, распределяемые в равных долях между лучшими пятью участниками.</div>
                                 <div class="inform">
-                                <div class="inform__title">Информация</div>
-                                <ul>
-                                    <li class="inform__body">Дата проведения: 12.06.2022 - 29.07.2022</li>
-                                </ul>
-                            </div>
+                                    <div class="inform__title">Информация</div>
+                                    <div class="inform__body">Дата проведения: <span class="inform__dateFromBase">12.06.2022 - 29.07.2022</span></div>
+                                </div>
                         </div>
+                    </div>
+                    </div>
+                </div>
+            </section> -->
+
+
+            <section class="nezavisimaya-expertiza">
+                <div class="container">
+                    <div class="events__list">
+                        <div class="events__item event-item">
+                            <div class="event-item__body">
+                                <div class="event-item__label">Развитие спорта в Оренбургской области</div>
+                            </div>
+                        <div class="event-item__info">
+                            <div class="event-item__status">Статус: <span></span></div>
+                        </div>
+                        
                     </div>
                     </div>
                 </div>
